9dd36eb36d37b9836bbe7eb53567b08a8ea99722
Alexandre Bardot <bardotalexandre@gmail.com>
2020-03-11 18:33:53 +0100

    second try

---
+++ src/components/Contact.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export default class Contact extends Component {
+  state = {
+    show: true
+  };
+
+  supprimeContact = () => {
+    this.props.supprimeClick()
+  }
+
+  montrerContact = () => {
+    this.setState({
+      show: !this.state.show});
+  };
+
+  render() {
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {this.props.nom}&nbsp;
+          <i style={{cursor:'pointer'}} className="fas fa-sort-down" onClick={this.montrerContact}></i>
+          <i className="fas fa-times" style={{cursor: 'pointer', float: 'right',
+          color:'red'}} onClick={this.supprimeContact} ></i>
+        </h4>
+        {this.state.show ? (
+          <ul className="card card-body mb-3">
+            <li className="list-group-item">Email :{this.props.email}</li>
+            <li>Téléphone : {this.props.tel} </li>
+          </ul>
+        ) : null}
+      </div>
+    );
+  }
+}
